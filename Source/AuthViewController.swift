235c022607b668f041c31a55b021b4dec69664fb
MaxiAir <mstegnienko@agranovsky.org>
2020-02-24 23:58:43 +0300

    create initial auth VC

--- Source/AuthViewController.swift
+++ Source/AuthViewController.swift
@@ -8,8 +8,38 @@
 import UIKit
 
 public final class AuthViewController: UIViewController {
+    // MARK: - Private properties
+    
+    private let loginTextField = UITextField()
+    private let passwordTextField = UITextField()
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setupLayout()
+    }
+}
+
+// MARK: - Private
+
+private extension AuthViewController {
+    enum Layout {
+        static let topOffset: CGFloat = 50
+        static let textFieldSize = CGSize(width: 200, height: 40)
+    }
+    
+    func setupLayout() {
+        view.backgroundColor = .white
+        
+        loginTextField.placeholder = "Login"
+        loginTextField.textAlignment = .left
+        loginTextField.textColor = .black
+        loginTextField.keyboardType = .emailAddress
+        
+        view.addSubview(loginTextField)
+        NSLayoutConstraint.activate([
+            loginTextField.topAnchor.constraintEqualToSystemSpacingBelow(view.topAnchor, multiplier: Layout.topOffset),
+            loginTextField.heightAnchor.constraint(equalToConstant: Layout.textFieldSize.height),
+            loginTextField.widthAnchor.constraint(equalToConstant: Layout.textFieldSize.width)
+        ])
     }
 }
