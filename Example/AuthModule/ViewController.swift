94c92c86025e064780d851cd12e91bb1af6e08c4
MaxiAir <mstegnienko@agranovsky.org>
2020-02-24 12:01:52 +0300

    Initial commit

---
+++ Example/AuthModule/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AuthModule
+//
+//  Created by MaxiJunkie on 02/24/2020.
+//  Copyright (c) 2020 MaxiJunkie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
