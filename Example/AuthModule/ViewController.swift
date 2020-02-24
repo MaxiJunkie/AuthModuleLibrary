2680468b9037fa32e8119635e16ec72da764c67d
MaxiAir <mstegnienko@agranovsky.org>
2020-02-24 22:11:01 +0300

    auth module added

--- Example/AuthModule/ViewController.swift
+++ Example/AuthModule/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AuthModule
 
 class ViewController: UIViewController {
 
---
+++ Example/Module/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  AuthModule_Example
+//
+//  Created by Максим Стегниенко on 24.02.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
